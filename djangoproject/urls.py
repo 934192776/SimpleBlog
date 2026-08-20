06ff110d90f5922f320a16a649c954c4167fec33
Jing Hui <934192776@qq.com>
2026-08-21 09:30:50 +1200

    Update urls.py

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include("polls.urls")
-        path('', include("blog.urls"),
+    path('polls/', include("polls.urls"))
+        path('', include("blog.urls")),
 ]
